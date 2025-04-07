c1988c474f54bb157c9537c80ce16610596126e3
hmaciasn <hector.a.macias@gmail.com>
2025-04-07 14:05:21 -0600

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Widgets Básicos',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Ejemplo Widgets Flutter'),
         ),
         body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Texto con Widget Text'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.star, color: Colors.blue),
+                  Text('Fila con Row')
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.amber,
+                child: Text('Container con Padding'),
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Text('Stack de Widgets'),
+                ],
+              ),
+            ],
           ),
         ),
       ),
